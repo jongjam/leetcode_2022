cbf6ae7c289f8a51a4548b770c77ad7d41ebe5e1
jongjam <jongjam64@gmail.com>
2023-10-11 10:58:06 +0900

    added stream array

--- kth_largest_lc703.py
+++ kth_largest_lc703.py
@@ -1,12 +1,17 @@
 class KthLargest:
 
-    def __init__(self, k: int, nums: List[int]):
+    def __init__(self, k, nums):
+        first = [[k, nums]]
+        print(first)
+        first.append([10])
+        print(first)
+
+    # def add(self, val: int) -> int:
         
 
-    def add(self, val: int) -> int:
-        
-
-
+def main() :
+    obj = KthLargest(3, [4, 5, 8, 2])
+main()
 # Your KthLargest object will be instantiated and called as such:
 # obj = KthLargest(k, nums)
 # param_1 = obj.add(val)

ee6816853dbb38dc58a24f34b9957b52873d1739
jongjam <jongjam64@gmail.com>
2023-10-11 11:03:00 +0900

    oop version of stream complete

--- kth_largest_lc703.py
+++ kth_largest_lc703.py
@@ -1,16 +1,17 @@
 class KthLargest:
-
     def __init__(self, k, nums):
-        first = [[k, nums]]
-        print(first)
-        first.append([10])
-        print(first)
+        self.stream = [[k, nums]]
 
-    # def add(self, val: int) -> int:
-        
+    def add(self, val) -> int:
+        self.stream.append([val])
+    
+    def get_stream(self) :
+        print(self.stream)
 
 def main() :
     obj = KthLargest(3, [4, 5, 8, 2])
+    obj.add(10)
+    obj.get_stream()
 main()
 # Your KthLargest object will be instantiated and called as such:
 # obj = KthLargest(k, nums)

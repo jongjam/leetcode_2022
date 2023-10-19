6d7f035c39b0ea836e2a13887807aef36745ca2e
jongjam <jongjam64@gmail.com>
2023-10-19 16:11:57 +0900

    complete solution using quick select

--- kth_largest_inarray_lc215.py
+++ kth_largest_inarray_lc215.py
@@ -1,52 +1,38 @@
 class Solution:
-    # Quick sort.... can use sort python thingy but no one will just
-    # let that shit slide GOD FUCKING DAMNIT!
-    og_len = 0
-    idx = 0
-    
-    def partition(self, nums, start, end) :
-        i = start - 1 # I is the location of the larger element and will only go up if j passes over a larger item
-        j = start
-
-        pivot = end
-
-        while j < end :
-            if nums[j] < nums[pivot] :
-                i += 1
-                nums[j], nums[i] = nums[i], nums[j]
-            j += 1
+    def findKthLargest(self, nums, k):
+        # Guess it can just pull nums straight in ?
+         # Getting the pivot at the end of the array
         
-        i += 1
-        nums[i], nums[pivot] = nums[pivot], nums[i]
-
-        return i # I is the location of the old pivot and partitions will continue based on this location
-
-
-    def quick_select(self, nums, start, end, k) : 
-        if end <= start :
-            print('help')
-            return nums[self.idx]
-        self.partition(nums, start, end) # Problem I don't use pivot
-        half = self.og_len // 2
-
-        if k < half :
-            self.quick_select(nums, self.idx + 1, end, k)
-        elif k > half :
-            self.quick_select(nums, start, self.idx - 1, k)
-        else :
-            print('gotcha bitch')
-            return nums[self.idx]
-
-    def findKthLargest(self) -> int:
-        # It is quick sort except the next partion will happen
-            # if k less than half nums length... start length - k to end
-            # if k greater than half nums length... start 0 to length - k
-        nums = [2, 4, 1, 3]
-        k = 2
+        # For simplicity changing K to get rid of any of that idx bulshit
+        k = len(nums) - k
+        # Left and right are start and end
         
-        self.idx = len(nums) - k
-        self.og_len = len(nums)
-        self.quick_select(nums, 0, len(nums) - 1, k)
+        def quick_select(start, end) :
+            # l and right are the bounds... don't try and remember just do wwhat you know
+            # i is like j it will go up no matter what
+            i = start
+            pivot = end
+            for j in range(start, end) : # stops before the pivot duh
+                if nums[j] <= nums[pivot] :            
+                    nums[j], nums[i] = nums[i], nums[j]
+                    i += 1
+                    
+          
+            nums[i], nums[pivot] = nums[pivot], nums[i]
+            pivot = i # The pivot location is now where i is
+            # Updating this accordingly for ease of readability
+            if pivot < k : # look to the right 
+                return quick_select(pivot + 1, end)
+            elif pivot > k : 
+                return quick_select(start, pivot - 1)
+            else :
+                print(nums)
+                return nums[pivot]
+        
+        return quick_select(0, len(nums) - 1)
     
-
-   
+def main() :
+    nums = [3,2,1,5,6,4]
+    k = 2
+    print(Solution.findKthLargest(nums, k))
+    print(nums)

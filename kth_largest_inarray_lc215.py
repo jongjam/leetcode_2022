5ce6204e0ffdaa4f40047b96fe86395ef7c3588f
jongjam <jongjam64@gmail.com>
2023-10-19 16:24:05 +0900

    quick select... lc215

--- kth_largest_inarray_lc215.py
+++ kth_largest_inarray_lc215.py
@@ -1,5 +1,5 @@
 class Solution:
-    def findKthLargest(self, nums, k):
+    def findKthLargest(self, nums: List[int], k: int) -> int:
         # Guess it can just pull nums straight in ?
          # Getting the pivot at the end of the array
         
@@ -10,29 +10,23 @@ class Solution:
         def quick_select(start, end) :
             # l and right are the bounds... don't try and remember just do wwhat you know
             # i is like j it will go up no matter what
-            i = start
-            pivot = end
+            p = start
+            pivot = nums[end]
             for j in range(start, end) : # stops before the pivot duh
-                if nums[j] <= nums[pivot] :            
-                    nums[j], nums[i] = nums[i], nums[j]
-                    i += 1
-                    
-          
-            nums[i], nums[pivot] = nums[pivot], nums[i]
-            pivot = i # The pivot location is now where i is
+                if nums[j] <= pivot :            
+                    nums[j], nums[p] = nums[p], nums[j]
+                    p += 1
+                
+            nums[p], nums[end] = nums[end], nums[p]
+             # The pivot location is now where i is
             # Updating this accordingly for ease of readability
-            if pivot < k : # look to the right 
-                return quick_select(pivot + 1, end)
-            elif pivot > k : 
-                return quick_select(start, pivot - 1)
+
+            # P or I is where the pivot will end up by the end
+            if p > k : 
+                return quick_select(start, p - 1)
+            elif p < k : # look to the right 
+                return quick_select(p + 1, end)
             else :
-                print(nums)
-                return nums[pivot]
+                return nums[p]
         
         return quick_select(0, len(nums) - 1)
-    
-def main() :
-    nums = [3,2,1,5,6,4]
-    k = 2
-    print(Solution.findKthLargest(nums, k))
-    print(nums)

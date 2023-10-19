85b1359d7fb85e51b61a782b17c502322e192015
jongjam <jongjam64@gmail.com>
2023-10-19 15:21:41 +0900

    unsucessful idea of quickselect approach

--- kth_largest_inarray_lc215.py
+++ kth_largest_inarray_lc215.py
@@ -3,25 +3,14 @@ class Solution:
     # let that shit slide GOD FUCKING DAMNIT!
     og_len = 0
     idx = 0
-
-    def quick_select(self, nums, start, end, k) : 
-        if end <= start :
-            return nums[self.idx]
-        pivot = partition(nums, start, end)
-        half = self.og_len // 2
-
-        if k < half :
-            quick_select(nums, self.idx, end, k)
-        if k >= half :
-            quick_select(nums, start, self.idx, k)
-
+    
     def partition(self, nums, start, end) :
         i = start - 1 # I is the location of the larger element and will only go up if j passes over a larger item
         j = start
 
         pivot = end
 
-        while j <= end :
+        while j < end :
             if nums[j] < nums[pivot] :
                 i += 1
                 nums[j], nums[i] = nums[i], nums[j]
@@ -33,13 +22,31 @@ class Solution:
         return i # I is the location of the old pivot and partitions will continue based on this location
 
 
+    def quick_select(self, nums, start, end, k) : 
+        if end <= start :
+            print('help')
+            return nums[self.idx]
+        self.partition(nums, start, end) # Problem I don't use pivot
+        half = self.og_len // 2
 
-    def findKthLargest(self, nums: List[int], k: int) -> int:
+        if k < half :
+            self.quick_select(nums, self.idx + 1, end, k)
+        elif k > half :
+            self.quick_select(nums, start, self.idx - 1, k)
+        else :
+            print('gotcha bitch')
+            return nums[self.idx]
+
+    def findKthLargest(self) -> int:
         # It is quick sort except the next partion will happen
             # if k less than half nums length... start length - k to end
             # if k greater than half nums length... start 0 to length - k
+        nums = [2, 4, 1, 3]
+        k = 2
+        
         self.idx = len(nums) - k
         self.og_len = len(nums)
-        return quick_select(nums, 0, len(nums) - 1, k)
+        self.quick_select(nums, 0, len(nums) - 1, k)
+    
 
    

e5ed412b6d5c2e6c18797d78ac65f228e853da83
jongjam <jongjam64@gmail.com>
2023-11-10 11:19:52 +0900

    completed minstack using a second stack to keep track of mins versus an array

--- stack/minstack.py
+++ stack/minstack.py
@@ -3,30 +3,30 @@ class MinStack:
     
 
     def __init__(self):
-        self.size = 0
-        self.minlocation = []
         self.stack = []
-        self.minval = float('inf')
+        self.mins = []
 
     def push(self, val: int) -> None:
+        curmin = self.getMin()
+        if val < curmin :
+            curmin = val
         self.stack.append(val)
-        self.minval = min(val, self.minval)
-        self.minlocation.append(self.minval)
-        self.size += 1
+        self.mins.append(curmin)
+        
 
     def pop(self) -> None:
         self.stack.pop()
-        self.size -= 1
-        if self.size == 0 :
-            self.minval = float('inf')
-            self.minlocation = []
-        #find new minval
+        self.mins.pop()
 
     def top(self) -> int:
-        return self.stack[len(self.stack) - 1]
+        if len(self.stack) == 0 :
+            return None
+        return self.stack[-1]
 
     def getMin(self) -> int:
-        return self.minlocation[self.size - 1]
+        if len(self.mins) == 0 :
+            return float('inf')
+        return self.mins[-1]
         # how do i find a new minval after a pop
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
@@ -34,6 +34,5 @@ class MinStack:
 # obj.pop()
 # param_3 = obj.top()
 # param_4 = obj.getMin()
-
 
 

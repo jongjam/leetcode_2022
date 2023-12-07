96d232e8de73cdfc0efc5262282279c00bdd135d
jongjam <jongjam64@gmail.com>
2023-12-07 01:03:06 -0800

    starting permutation in string

---
+++ sliding_window/permutation_in_string.py
@@ -0,0 +1,10 @@
+class Solution:
+    def checkInclusion(self, s1, s2) :
+        # a permutation is just another way to write a string
+
+
+        # keep moving r window as long as s2[r] is in set of s1
+        # and the occurence of s2[r] iis not greater than letter cout in s1
+      
+        return False
+

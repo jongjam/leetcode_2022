5f2865c5cb086ee3d311432e957a2ae754bc39b8
jongjam <jongjam64@gmail.com>
2024-03-28 22:42:21 -0700

    rotting_oranges half of test cases passed but need a different approach for calculating minutes

--- graphs/rotting_oranges.py
+++ graphs/rotting_oranges.py
@@ -6,27 +6,41 @@ class Solution:
         # If so add them to queue and change them to 2, marking them as visited
         # figure out how to increase time only per level
 
-        # Minutes cannot be calculated by level.
-        # need to find the max distance between the furthest fresh orange from the first rotten one
-        minutes = 0 
+        # Minutes is really the x + y distance from the last level of oranges
+        
+      
+        
+        ROWS = len(grid)
+        COLS = len(grid[0])
+
+        def in_bounds(x, y) :
+            a = x in range(ROWS) and y in range(COLS)
+            print(a)
+            return a
         
         # LRUD
         directions = ((-1,0), (1,0), (0,1), (0,-1))
+        # minute calculations are wrong and likely need some kind of dfs 
+        # haave to find the deepest orange from the rotten orange origin 
 
+        # need to find the max distance between the furthest fresh orange from the first rotten one
+        minutes = [0]
+        
         def bfs(i, j) :
             q = deque()
             q.append((i,j))
 
             while q:
-                i, j = q.popleft()
-                
+                x, y = q.popleft()
                 for direction in directions :
-                    next_i = i + direction[0]
-                    next_j = j + direction[1]
-                    if grid[next_i][next_j] == '1' :
-                        grid[next_i][next_j] = '2'
-                        q.append((next_i, next_j))
-                minutes += 1
+                    next_x = x + direction[0]
+                    next_y = y + direction[1]
+
+                    if in_bounds(next_x, next_y) and grid[next_x][next_y] == 1 :
+                        grid[next_x][next_y] = 2 
+                        print(grid[next_x][next_y])
+                        minutes[0] = max(minutes[0], abs(next_x - i) + abs(next_y - j))
+                        q.append((next_x, next_y))
 
         for i in range(len(grid)) :
             for j in range(len(grid[i])) :
@@ -38,4 +52,4 @@ class Solution:
                 if grid[i][j] == 1 :
                     return -1
 
-        return minutes
+        return minutes[0]

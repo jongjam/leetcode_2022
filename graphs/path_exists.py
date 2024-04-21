3bc6bb0e4b9f5a74f5f54b8b0155567b42045baa
jongjam <jongjam64@gmail.com>
2024-04-21 01:42:31 -0700

    graph searching for path

---
+++ graphs/path_exists.py
@@ -0,0 +1,33 @@
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        # given list of edges
+        # there are cycles but no cycles pointing to oneself.
+        # finding a path from source to destination I can use either bfs or dfs
+        if source == destination :
+            return True
+        adj_list = {i:[] for i in range(n)}
+        for src,dest in edges :
+            adj_list[src].append(dest)
+
+        
+
+        # start the algorithm starting at source 
+        stack = deque()
+        stack.append(source)
+        visited = set()
+
+        while stack :
+            current = stack.pop()
+            visited.add(current)
+            for neighbor in adj_list[current] :
+                if neighbor == destination :
+                    return True
+                if neighbor not in visited :             
+                    stack.append(neighbor)
+
+        return False
+
+
+
+
+

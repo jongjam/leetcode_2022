7b38f6f5b6792892bc1c39de1992afc025ee4f79
Jimmy Jhong <48465858+jongjam@users.noreply.github.com>
2024-04-24 13:28:11 -0700

    Algorithm split to run on different bounds for even and odd strings

--- dynamic_programming_oned/longest_palind_substring.py
+++ dynamic_programming_oned/longest_palind_substring.py
@@ -1,33 +1,31 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        # longest palindrome is the longest substring palindrome 
-        
-        # starting from the middle, check if one to the left is good, check one to the right is good
-        # check if both
-        # isn't this just fucking two pointer FUCK YOU !
+        result = ""
+        resLen = 0
+        def in_range(l, r) :
+            return l >= 0 and r < len(s)
 
-        mid =(int)((len(s) - 1) / 2)
-        result = s[mid]
-        left = mid - 1
-        right = mid + 1
-        print(result)
-        def inRange(left, right) :
-            return left >= 0 or right < len(s)
-        
-        while inRange(left, right) :
-            
-            left_char = s[left] if left >= 0 else None
-            right_char = s[right] if right < len(s) else None
-            if left_char == right_char :
-                result = left_char + result + right_char
-            else :
-                if left_char == result[len(result) - 1] :
-                    result = left_char + result
-                if right_char == result[0] :
-                    result = result + right_char
-            
-            left -= 1
-            right += 1
-            
-            
+        for i in range(len(s)) :
+            # Even length
+            l, r = i, i
+            while in_range(l, r) and s[l] == s[r] :
+                if (r - l + 1) > resLen :
+                    result = s[l:r + 1]
+                    resLen = r - l + 1
+                l -= 1
+                r += 1
+            # Uneven length
+            l, r = i, i + 1 # starting one over so let's say it starts at 
+            # i = 1, first b in example 2
+            # going r = i + 1
+            # will look at bb and then call it good 
+            while in_range(l, r) and s[l] == s[r] :
+                if (r - l + 1) > resLen :
+                    result = s[l:r + 1]
+                    resLen = r - l + 1
+                    print(resLen)
+                    print(len(result))
+                l -= 1
+                r += 1
+
         return result
